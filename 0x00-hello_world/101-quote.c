9b1c684a26b8f7c690cee5f6458d20715994cdd9
Yusuf <yuzer5252@gmail.com>
2023-10-05 12:44:39 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main()
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	const char *message = "and that piece of art is useful
+                        \" - Dora Korpar, 2015-10-19\n";
+	ssize_t bytes_written = write(STDERR_FILENO, message, 15);
+	return (bytes_written == 15) ? 0 : 1;
 }
