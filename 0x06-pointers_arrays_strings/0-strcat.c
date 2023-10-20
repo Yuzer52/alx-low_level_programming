c255510770b75bb1939563ce244d48a5d62f0b5f
Yusuf <yuzer5252@gmail.com>
2023-10-20 13:23:32 -0700

    0-strcat1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - copies data
- * @s: the source to copy it to
- * @b: the number of bytes to be overwritten
- * @n: integer variable
- *
- * Return: Nothing.
- */
+  * _strcat: Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
 char *_strcat(char *dest, char *src)
 {
-	unsigned int a;
-	unsigned int b;
-	unsigned int n;
+	int dlength = 0, i;
 
-	for (a = 0; a < n; a++)
-		src[a] = b;
+	while (dest[dlength])
+	{
+		dlength++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlength] = src[i];
+		dlength++;
+	}
+
+	dest[dlength] = '\0';
 	return (dest);
 }
