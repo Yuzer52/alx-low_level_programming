b5801babd951cb07e2d10bdf01a5e2de9e5181a9
Yusuf <yuzer5252@gmail.com>
2023-10-20 13:11:27 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - copies data
+ * @s: the source to copy it to
+ * @b: the number of bytes to be overwritten
+ * @n: integer variable
+ *
+ * Return: Nothing.
+ */
+char *_strcat(char *dest, char *src)
+{
+	unsigned int a;
+	unsigned int b;
+	unsigned int n;
+
+	for (a = 0; a < n; a++)
+		src[a] = b;
+	return (dest);
+}
