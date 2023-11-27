cf08c0e8b4cee8d4a553cc18f847cad0fa4c0838
Yusuf <yuzer5252@gmail.com>
2023-11-27 11:36:25 -0800

    commited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,31 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - It will Updates the value it points to 98
-  * @n: A pointer to n
+  * read_textfile - reads a text file and prints it to the POSIX standard output
+  * @filename: Source file
+  * @letters: Number of letters to reads and prints
   *
-  * Return: Nothing
+  * Return: readed
   */
-void reset_to_98(int *n)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	*n = 98;
-}
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
+	if (!buff)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
+}
