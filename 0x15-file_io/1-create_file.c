75e90bee5120d65c043da3967413048857646ed7
Yuzer52 <yuzer5252@gmail.com>
2024-06-08 04:42:50 -0700

    all done please review

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,38 @@
 #include "main.h"
 
 /**
-  * create_file - It creates a file
-  * @filename: The name of the file to create
-  * @text_content: A NULL terminated string to write to the file
-  *
-  * Return: if filename is NULL return -1
-  */
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
-
-/**
-  * _strlen - The length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
-}
-
