24b99652161f505452a6f84808ed49afc2a8ea8e
Yuzer52 <yuzer5252@gmail.com>
2024-06-08 04:48:17 -0700

    please review this

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
+ * error_file - It will checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
@@ -23,12 +23,10 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- *
- * main - check the code for students.
+ * main - check the code for Holberton School students.
  * @argc: number of arguments.
  * @argv: arguments vector.
- * Return: Always 0
- *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
